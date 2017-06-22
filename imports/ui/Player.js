a3ee1e82472a2172c2105ab46f6121dba61db17e
Gabriel Erdelyi <erdgabios@gmail.com>
2017-06-22 18:34:31 +0200

    Second commit

---
+++ imports/ui/Player.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Tracker} from 'meteor/tracker';
+import PropTypes from 'prop-types';
+
+import {Players} from './../api/players';
+
+export default class Player extends React.Component {
+  render() {
+    return (
+        <div key={this.props.player._id} className="item">
+          <div className="player">
+            <div>
+              <h3 className="player__name">{this.props.player.name}</h3>
+              <p className="player__stats">
+                {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s).
+              </p>
+            </div>
+            <div className="player__actions">
+              <button className="button button--round" onClick={() => {
+                Players.update({_id: this.props.player._id}, {
+                  $inc: {score: -1}
+                });
+              }}>-1</button>
+              <button className="button button--round" onClick={() => {
+                Players.update({_id: this.props.player._id}, {
+                  $inc: {score: 1}
+                });
+              }}>+1</button>
+              <button className="button button--round" onClick={() => Players.remove({_id: this.props.player._id})}>X</button>
+            </div>
+          </div>
+        </div>
+    );
+  }
+};
+
+// PropTypes.object
+
+Player.propTypes = {
+  player: PropTypes.object.isRequired
+}
